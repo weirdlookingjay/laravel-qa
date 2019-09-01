38b7efa51e5b91a428d98303a23d109698a3351b
Joe Clark <weirdlookingjay@live.comm>
2019-09-01 18:50:39 -0400

    Populate users & questions tables usimg model factories and seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(User::class, 3)->create()->each(function($u) {
+            $u->questions()->saveMany(
+                factory(App\Question::class, rand(1, 5))->make()
+            );
+        });
+
     }
 }
